d4f670407aca01bbcda47fbe6d1286ec550067d0
iiita <anoj.kumar48@gmail.com>
2018-09-04 16:34:55 +0530

    assignment-4

--- assignment-2/linklist_string.c
+++ assignment-2/linklist_string.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct node {
 	char string[100];
@@ -9,7 +10,8 @@ struct node {
 
 struct node * create_linklist(char c[]){
 	struct node *head = (struct node *)malloc(sizeof(struct node));
-	head -> string = c;
+	//head -> string = c;
+	strcpy(head->string, c);
 	head -> next = NULL;
 	return head;
 }
@@ -18,19 +20,20 @@ void add_node_last(struct node * head , char c[]){
 	struct node *t =head;
 
 	struct node *p = (struct node *)malloc(sizeof(struct node));
-	p -> string = c;
+	strcpy( p->string, c);
+	//p -> string = c;
 	while ( t->next != NULL ){
 		t = t -> next;
 	}
 		t ->next = p;
 		p -> next = NULL;
-		p -> string = c;
+		//p -> string = c;
 	
 }
 void print_array(struct node * head){
 	struct node * t = head;
 	while ( t != NULL){
-		prinf("%s\n", t->string);
+		printf("%s\n", t->string);
 		t = t -> next;
 	}
 }
--- assignment-4/hashing.c
+++ assignment-4/hashing.c
@@ -3,18 +3,24 @@
 #define SIZE  10
 
 struct node {
+	
 	int data;
 	struct node * next;
+
 };
 
 struct node * get_node(){
+	
 	struct node *t = (struct node *)malloc(sizeof(struct node));
 	return t;
+
 }
 
 int calculate_hash(int num){
+	
 	int r = num % SIZE;
 	return r;
+
 }
 
 
@@ -44,11 +50,68 @@ void add_node_to_hash(struct node  *arr[], int index, int num){
 }
 
 
-void hashing_by_chaning(struct node * arr[], int num){
+void hashing_by_chaining(struct node * arr[], int num){
+
 	int hash_index_num = calculate_hash(num);
-//	printf("\nHash index %d, %d\n", hash_index_num, num );
 	add_node_to_hash(arr, hash_index_num, num);
 	
+
+}
+
+void delete_node_chaining(struct node *arr[], int num){
+
+	int hash_index_num =  calculate_hash(num);
+	struct node *t = arr[hash_index_num];
+	struct node * temp;
+	
+	if (t -> data == num){	
+		temp = t;
+		arr[hash_index_num] = t -> next;
+		free(temp);
+
+	} else {
+	
+		while(t -> next != NULL){
+			if (t -> next -> data == num ){
+				temp = t -> next;
+				t -> next = t -> next -> next;
+				free(temp);
+				return;
+			}
+	
+			t = t -> next;
+	
+		}
+
+		printf("\nElement not found\n");
+	}
+
+
+}
+
+
+void search_number(struct node *arr[], int num){
+
+	int hash_index_num = calculate_hash(num);
+	struct node * t = arr[hash_index_num];
+	
+	if (t != NULL){
+		
+		while (t != NULL){
+			if (t -> data == num){
+				printf("\nElement %d is found\n", num);
+				return;
+			}
+			t = t -> next;
+		}
+
+		printf("\nElement not found\n");
+
+	} else {
+
+		printf("\nElement not found\n");
+
+	}
 
 }
 
@@ -62,22 +125,44 @@ void print_hash(struct node *arr[]){
 			t = t -> next;
 		}		
 	}
+	printf("\n");
 
 }
 
 void main(){
+
 	struct node * a[SIZE];
+
 	for ( int i = 0; i < SIZE; i++ ){
 		a[i] = NULL;
 	}
+
 	int n;
 	scanf("%d", &n);
 
 	for ( int i = 0 ; i < n; i++){
 		int num;
 		scanf("%d", &num);
-		hashing_by_chaning(a, num);
+		hashing_by_chaining(a, num);
 	}
 
 	print_hash(a);
+	printf("\n");
+
+	int d;
+	scanf("%d", &d);
+	for (int i = 0; i < d; i++){
+		int d_num;
+		scanf("%d", &d_num);
+		delete_node_chaining(a, d_num);
+	}
+
+	print_hash(a);
+	printf("\n");
+
+	int s;
+	scanf("%d", &s);
+	search_number(a, s);
+
+
 }
