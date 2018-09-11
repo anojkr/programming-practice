9b0df307bae814d4d98981fd7dbffc862bcdf906
iiita <anoj.kumar48@gmail.com>
2018-09-11 17:43:19 +0530

    assignment -4

---
+++ pp/assignment-4/q_4.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void caculate_hash(int p[26], char a[100]){
+
+	for (int i = 0; i < 20; i++){
+
+		if (a[i] == 'a' || a[i] == 'e'|| a[i] == 'i' || 
+				a[i] == 'o' || a[i] == 'u'){
+		int hash = a[i] - 'a';
+//		printf("%c =  %d", a[i], hash);
+//		printf("\n");
+		p[hash] = p[hash] + 1;
+		}
+	
+	}
+
+}
+
+void print_ll(int p[26]){
+	int c = 0;
+	for (int i = 0; i < 26; i++){
+		if (p[i] == 1){
+			printf("%c ", i+'a');
+			c = 1;
+		}
+	}
+	if (c == 0 ){
+		printf("NONE");
+	}
+	printf("\n");
+	
+}
+
+void main(){
+
+	int n;  
+	scanf("%d", &n);
+
+	int hash_table[26];
+
+	
+	
+	while(n){
+
+		for(int i=0; i<26; i++){
+			hash_table[i] = 0;
+		}
+
+		char str[100];
+		scanf("%s",str);
+		caculate_hash(hash_table, str);
+		n--;
+	}
+	
+	print_ll(hash_table);
+
+
+}
